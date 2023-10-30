cbeb4445c60fd5ae245e55357a8e3f236acd1482
furozka <furozka05.github@gmail.com>
2023-10-30 06:31:58 +0300

    ejs applied, partials, views, public folders setted

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
+const ejs = require("ejs")
 
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-    const blog = {
-        id: 1,
-        title: 'Blog title',
-        description: 'Blog description',
-    };
-    res.send(blog);
+    res.render('index');
+});
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+app.get('/add_post', (req, res) => {
+    res.render('add_post');
 });
 
 const port = 3000;
