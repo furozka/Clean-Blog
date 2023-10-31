a68a710a8aa7874f110acc3485ee7d76c87252df
furozka <furozka05.github@gmail.com>
2023-10-31 15:54:41 +0300

    mongoose model created and connected to mongodb

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const mongoose = require("mongoose")
 const app = express();
 const ejs = require("ejs")
+const Post = require("./models/Post");
+const { redirect } = require('react-router-dom');
+
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect("mongodb://localhost/cleanblog-test-db")
+
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     res.render('index');
@@ -13,6 +22,11 @@ app.get('/about', (req, res) => {
 app.get('/add_post', (req, res) => {
     res.render('add_post');
 });
+
+app.post('/posts', async (req,res)=>{
+    const data = await Post.create(req.body)
+    res.redirect("/")
+})
 
 const port = 3000;
 app.listen(3000, () => console.log('blog server is uppp'));
